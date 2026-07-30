d4090786f67ed8d277b9c91118f4f8a0948777f7
Joe Starr <52646388+Joecstarr@users.noreply.github.com>
2026-07-30 14:00:53 -0500

    [🏗️] Adds pypi publishing (#6)

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,8 @@
 """Marks the package containing the Fastrak driver."""
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = '0.0.0'  # Fallback for development mode
